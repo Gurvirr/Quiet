15fa234b2698e2847edfd7cb12954b3fec6544a4
Gurvir <gurvirrandhawa28@gmail.com>
2025-06-25 23:14:21 -0400

    init: electron-app configuration

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// entry point for Electron
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.setMenuBarVisibility(false);
+
+  win.loadFile("src/frontend/index.html");
+}
+
+app.whenReady().then(createWindow);
