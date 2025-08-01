c73b3f6b7dba424ed8dfc5e57a9dd8f4826a49e6
Gurvir <gurvirrandhawa28@gmail.com>
2025-08-01 11:04:40 -0400

    style: improve window styling

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <QApplication>
-#include <QMainWindow>
+#include <QWidget>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    QMainWindow window;
+    QWidget window;
+    window.setWindowOpacity(0.3);
+    window.setStyleSheet("background-color: rgb(0, 0, 0);");
+    window.resize(1338, 752);
     window.show();
     return app.exec();
 }
